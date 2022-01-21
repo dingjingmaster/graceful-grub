2e6ffdfa6f1b29329069ef11319e3c8a30d32a77
dingjing <dingjing@live.cn>
2022-01-21 13:38:03 +0800

    grub-render-label

---
+++ util/grub-render-label.cmake
@@ -0,0 +1,16 @@
+FILE (GLOB GRUB_RENDER_LABEL_SRC
+    ${CMAKE_SOURCE_DIR}/util/grub-render-label.c
+    ${CMAKE_SOURCE_DIR}/util/render-label.c
+    ${CMAKE_SOURCE_DIR}/grub-core/kern/emu/argp_common.c
+    ${CMAKE_SOURCE_DIR}/grub-core/kern/emu/hostfs.c
+    ${CMAKE_SOURCE_DIR}/grub-core/disk/host.c
+    ${CMAKE_SOURCE_DIR}/grub-core/osdep/init.c
+    )
+source_group (grub-render-label ${GRUB_RENDER_LABEL_SRC})
+
+add_executable(grub-render-label ${GRUB_RENDER_LABEL_SRC})
+target_link_libraries (grub-render-label -ldevmapper -lgrubmods -lgrubgcry -lgrubkern -lgnu)
+target_include_directories (grub-render-label BEFORE PUBLIC ${CMAKE_SOURCE_DIR}/grub-core/lib/gnulib/)
+target_link_directories (grub-render-label BEFORE PUBLIC ${CMAKE_SOURCE_DIR}/.lib)
+set_target_properties (grub-render-label PROPERTIES OUTPUT_NAME "${CMAKE_BINARY_DIR}/grub-render-label")
+target_compile_definitions (grub-render-label PUBLIC -DGRUB_FILE=\"util/grub-render-label.c\")
